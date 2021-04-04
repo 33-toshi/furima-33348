f5eb46f85d328d55444469bdae1114a4535f60e6
33-toshi <hatta.satoshi.s610514@gmail.com>
2021-04-04 12:34:51 +0900

    ActiveHashの導入

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,15 +1,25 @@
 class Product < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   with_options presence: true do
     validates :product_name
     validates :description
-    validates :category_id
-    validates :detail_id
-    validates :delivery_fee_id
-    validates :area_id
-    validates :duration_id
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :detail_id
+      validates :delivery_fee_id
+      validates :area_id
+      validates :duration_id
+    end
     validates :price
     validates :image
   end
-
+  
   belongs_to :user
+  has_one_attached :image
+  belongs_to :category
+  belongs_to :detail
+  belongs_to :delivery_fee
+  belongs_to :area
+  belongs_to :duration
 end
