ae99a20121d257b10e1b7f5b68eea55205d21800
33-toshi <hatta.satoshi.s610514@gmail.com>
2021-04-09 15:49:58 +0900

    販売手数料、販売利益計算の実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,27 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+  
   def index
+    
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private 
+
+  def product_params
+    params.require(:product).permit(:product_name, :description, :category_id, :detail_id, :delivery_fee_id, :area_id, :duration_id, :price, :image).merge(user_id: current_user.id)
+  end
+  
 end
