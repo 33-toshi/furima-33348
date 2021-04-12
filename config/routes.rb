6b053770e2768e565de3b443dd23185b8b52c1d4
33-toshi <hatta.satoshi.s610514@gmail.com>
2021-04-12 17:19:28 +0900

    商品詳細表示の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   
   root to: 'products#index'
-  resources :products, only: [:index, :create, :new]
+  resources :products, only: [:index, :create, :new, :show]
 end
