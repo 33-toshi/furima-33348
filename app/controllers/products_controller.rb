442d3f6b477a923b980ff992a4aa82cb01e2ac47
33-toshi <hatta.satoshi.s610514@gmail.com>
2021-04-14 17:33:51 +0900

    Update products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :move_to_index, only: [:edit]
+  before_action :move_to_index, only: [:edit, :update]
+  before_action :set_product, only: [:show, :edit, :update]
 
   def index
     @products = Product.order('created_at DESC')
@@ -20,15 +21,12 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
   def edit
-    @product = Product.find(params[:id])
   end
 
   def update
-    @product = Product.find(params[:id])
     if @product.update(product_params)
       redirect_to product_path
     else
@@ -48,4 +46,9 @@ class ProductsController < ApplicationController
       redirect_to action: :index
     end
   end
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
 end
