a724ecdacbad1195e44678696b4c5d6c8a00085a
33-toshi <hatta.satoshi.s610514@gmail.com>
2021-04-14 15:16:12 +0900

    ログインかつ出品者のみ編集できる実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @products = Product.order('created_at DESC')
@@ -22,10 +23,29 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    product = Product.find(params[:id])
+    if product.update(product_params)
+      redirect_to product_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def product_params
     params.require(:product).permit(:product_name, :description, :category_id, :detail_id, :delivery_fee_id, :area_id,
                                     :duration_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in? && current_user.id == Product.find(params[:id]).user_id
+      redirect_to action: :index
+    end
   end
 end
