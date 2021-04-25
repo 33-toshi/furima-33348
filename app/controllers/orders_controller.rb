5f85476e485fde2e4654e3c757938186f2f67779
33-toshi <hatta.satoshi.s610514@gmail.com>
2021-04-25 21:30:00 +0900

    リダイレクトの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,12 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
 
   def index
     @product = Product.find(params[:product_id])
     @product_order = ProductOrder.new
+    if current_user == @product.user
+      redirect_to root_path
+    end
   end
 
   def create
@@ -18,18 +22,18 @@ class OrdersController < ApplicationController
       end
   end
 
-private
+  private
 
-  def order_params
-    params.require(:product_order).permit(:postal_code, :area_id, :city, :house_number, :building_name, :phone_number).merge(token: params[:token])
-  end
+    def order_params
+      params.require(:product_order).permit(:postal_code, :area_id, :city, :house_number, :building_name, :phone_number).merge(token: params[:token])
+    end
 
-  def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-    Payjp::Charge.create(
-      amount: @product.price,
-      card: order_params[:token],
-      currency: 'jpy'
-    )
-  end
+    def pay_item
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @product.price,
+        card: order_params[:token],
+        currency: 'jpy'
+      )
+    end
 end
