4525e4418582c0a0986ea13ce0b0ff8ecf67602e
33-toshi <hatta.satoshi.s610514@gmail.com>
2021-04-25 20:58:26 +0900

    クレジット決済機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,24 +2,34 @@ class OrdersController < ApplicationController
 
   def index
     @product = Product.find(params[:product_id])
-  end
-
-  def new
+    @product_order = ProductOrder.new
   end
 
   def create
+    @product = Product.find(params[:product_id])
     @product_order = ProductOrder.new(order_params)
+
       if @product_order.valid?
+        pay_item
         @product_order.save
-        redirect_to action: :index
+        redirect_to root_path
       else
-        render action: :new
+        render action: :index
       end
   end
 
 private
 
   def order_params
-    params.require(:product_order).premit(:postal_code, :area_id, :city, :house_number, :building_name, :postal_code)
+    params.require(:product_order).permit(:postal_code, :area_id, :city, :house_number, :building_name, :phone_number).merge(token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @product.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
--- app/models/product_order.rb
+++ app/models/product_order.rb
@@ -1,7 +1,7 @@
 class ProductOrder
 
   include ActiveModel::Model
-  attr_accessor :postal_code, :area_id, :city, :house_number, :building_name, :phone_number, :user_id, :product_id, :order_id
+  attr_accessor :postal_code, :area_id, :city, :house_number, :building_name, :phone_number, :user_id, :product_id, :order_id, :token
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
@@ -9,6 +9,7 @@ class ProductOrder
     validates :house_number
     validates :phone_number, format: { with: /\A\d{11}\z/
   }
+    validates :token
   end
   validates :area_id ,numericality: { other_than: 1 }
 
