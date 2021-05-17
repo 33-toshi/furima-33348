3fa5778b41984fcfe6549665bf947c4004b97cf9
33-toshi <hatta.satoshi.s610514@gmail.com>
2021-05-17 19:39:58 +0900

    マイページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   root to: 'products#index'
   resources :products do
     resources :orders, only:[:index, :create]
   end
-
+  resources :users, only: [:show, :update]
 end
